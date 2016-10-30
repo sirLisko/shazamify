b9bb1859dec712742c9e039391c41f4eb2112e85
Luca <sirlisko@gmail.com>
2016-10-30 16:21:01 +0000

    moved to eslint and standard configuration

--- chrome-extension/base.js
+++ chrome-extension/base.js
@@ -1,61 +1,61 @@
-(function(){
-	'use strict';
+(function () {
+  'use strict'
 
-	var urlBase = 'https://api.spotify.com/v1/search?type=track&q=', url;
+  var urlBase = 'https://api.spotify.com/v1/search?type=track&q='
+  var url
 
-	function handleResponse(data){
-		if(data && data.tracks && data.tracks.items && data.tracks.items.length) {
-			var button = document.createElement('a');
-			button.id = 'shazamify';
-			button.href = data.tracks.items[0].uri;
+  function handleResponse (data) {
+    if (data && data.tracks && data.tracks.items && data.tracks.items.length) {
+      var button = document.createElement('a')
+      button.id = 'shazamify'
+      button.href = data.tracks.items[0].uri
 
-			document.querySelector('.ctrl-bar > div').appendChild(button);
-		}
-	}
+      document.querySelector('.ctrl-bar > div').appendChild(button)
+    }
+  }
 
-	function apiOnLoad(e){
-		var data = e.target;
-		if (data.status >= 200 && data.status < 400){
-			data = JSON.parse(data.responseText);
-			handleResponse(data);
-		}
-	}
+  function apiOnLoad (e) {
+    var data = e.target
+    if (data.status >= 200 && data.status < 400) {
+      data = JSON.parse(data.responseText)
+      handleResponse(data)
+    }
+  }
 
-	function searchSpotify(url){
-		var request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.onload = apiOnLoad;
-		request.send();
-	}
+  function searchSpotify (url) {
+    var request = new XMLHttpRequest()
+    request.open('GET', url, true)
+    request.onload = apiOnLoad
+    request.send()
+  }
 
-	function getUrl(){
-		var track = document.querySelector('.trd-title').textContent +
-			' artist:' + document.querySelector('.trd-artist').textContent;
-		return urlBase + encodeURIComponent(track);
-	}
+  function getUrl () {
+    var track = document.querySelector('.trd-title').textContent +
+    ' artist:' + document.querySelector('.trd-artist').textContent
+    return urlBase + encodeURIComponent(track)
+  }
 
-	function waitForNewTrack(){
-		var waitTrack = setInterval(function(){
-			if(url !== getUrl()){
-				clearInterval(waitTrack);
-				fetchSpotify();
-			}
-		}, 100);
-	}
+  function waitForNewTrack () {
+    var waitTrack = setInterval(function () {
+      if (url !== getUrl()) {
+        clearInterval(waitTrack)
+        fetchSpotify()
+      }
+    }, 100)
+  }
 
-	function fetchSpotify(){
-		url = getUrl();
-		searchSpotify(url);
-	}
+  function fetchSpotify () {
+    url = getUrl()
+    searchSpotify(url)
+  }
 
-	function attachEvents() {
-		var tracks = document.querySelectorAll('[data-track-id] [href*="track"]');
-		Array.prototype.forEach.call(tracks, function(track){
-			track.addEventListener('click', waitForNewTrack, false);
-		});
-	}
+  function attachEvents () {
+    var tracks = document.querySelectorAll('[data-track-id] [href*="track"]')
+    Array.prototype.forEach.call(tracks, function (track) {
+      track.addEventListener('click', waitForNewTrack, false)
+    })
+  }
 
-	attachEvents();
-	fetchSpotify();
-
-})();
+  attachEvents()
+  fetchSpotify()
+})()
--- test/baseTest.js
+++ test/baseTest.js
@@ -1,80 +1,80 @@
 /* global sinon */
 
-(function(){
-	'use strict';
+(function () {
+  'use strict'
 
-	function createDOM(){
-		var fakeDOM = document.createElement('div');
-		fakeDOM.className = 'fakeDOM';
-		fakeDOM.innerHTML =
-			'<div class="tr-details">' +
-				'<h1 class="trd-title">foo</h1>' +
-				'<h2 class="trd-artist">bar</h2>' +
-			'</div>' +
-			'<div class="ctrl-bar"><div>';
+  function createDOM () {
+    var fakeDOM = document.createElement('div')
+    fakeDOM.className = 'fakeDOM'
+    fakeDOM.innerHTML =
+    '<div class="tr-details">' +
+    '<h1 class="trd-title">foo</h1>' +
+    '<h2 class="trd-artist">bar</h2>' +
+    '</div>' +
+    '<div class="ctrl-bar"><div>'
 
-		document.body.appendChild(fakeDOM);
-	}
+    document.body.appendChild(fakeDOM)
+  }
 
-	function loadExtension(callback){
-		var resource = document.createElement('script');
-		resource.src = 'base/chrome-extension/base.js';
-		resource.onload = callback;
-		var script = document.getElementsByTagName('script')[0];
-		script.parentNode.insertBefore(resource, script);
-	}
+  function loadExtension (callback) {
+    var resource = document.createElement('script')
+    resource.src = 'base/chrome-extension/base.js'
+    resource.onload = callback
+    var script = document.getElementsByTagName('script')[0]
+    script.parentNode.insertBefore(resource, script)
+  }
 
-	var server;
+  var server
 
-	module( 'shazamify', {
-	  setup: function() {
-	    server = sinon.fakeServer.create();
-		createDOM();
-	  },
-	  teardown: function() {
-	    server.restore();
-	    document.body.removeChild(document.querySelector('.fakeDOM'));
-	  }
-	});
+  module('shazamify', {
+    setup: function () {
+      server = sinon.fakeServer.create()
+      createDOM()
+    },
+    teardown: function () {
+      server.restore()
+      document.body.removeChild(document.querySelector('.fakeDOM'))
+    }
+  })
 
-	asyncTest('Check that the correct track info are passed to the Spotify API', 1, function() {
-		loadExtension(function(){
-			start();
-			ok(server.requests[0].url.indexOf('foo%20bar'), 'right query string appended');
-		});
-	});
+  asyncTest('Check that the correct track info are passed to the Spotify API', 1, function () {
+    loadExtension(function () {
+      start()
+      ok(server.requests[0].url.indexOf('foo%20bar'), 'right query string appended')
+    })
+  })
 
-	asyncTest('Spotify API returns no tracks', 1, function() {
-		loadExtension(function(){
-			start();
+  asyncTest('Spotify API returns no tracks', 1, function () {
+    loadExtension(function () {
+      start()
 
-			server.requests[0].respond(
-				200,
-				{ 'Content-Type': 'application/json' },
-				JSON.stringify({ tracks: [] })
-			);
+      server.requests[0].respond(
+        200,
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({ tracks: [] })
+      )
 
-			ok(!document.querySelectorAll('#shazamify').length, 'shazamify trigger has not been created!');
-		});
-	});
+      ok(!document.querySelectorAll('#shazamify').length, 'shazamify trigger has not been created!')
+    })
+  })
 
-	asyncTest('Spotify API returns tracks', 2, function() {
-		loadExtension(function(){
-			start();
+  asyncTest('Spotify API returns tracks', 2, function () {
+    loadExtension(function () {
+      start()
 
-			server.requests[0].respond(
-				200,
-				{ 'Content-Type': 'application/json' },
-				JSON.stringify({ tracks: {
-					items: [{
-						uri: 'bar'
-					}]
-				}})
-			);
+      server.requests[0].respond(
+        200,
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({ tracks: {
+          items: [{
+            uri: 'bar'
+          }]
+        }})
+      )
 
-			var shazamify = document.querySelectorAll('#shazamify');
-			ok(shazamify.length, 'shazamify trigger has been created!');
-			ok(shazamify[0].href, 'bar', 'with the correct href');
-		});
-	});
-})();
+      var shazamify = document.querySelectorAll('#shazamify')
+      ok(shazamify.length, 'shazamify trigger has been created!')
+      ok(shazamify[0].href, 'bar', 'with the correct href')
+    })
+  })
+})()
